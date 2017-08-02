9b5e5b06a184d8b4832e2910697982d23ab3fc49
liudewei <446841346@qq.com>
2017-08-02 17:38:15 +0800

    updated

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,5 +1,4 @@
 <script>
-import { mapGetters } from 'vuex';
 export default {
 };
 </script>
@@ -8,7 +7,21 @@ export default {
 <div class="about-mod">
 	<div class="about-dsc">
 		<h5 class="about-title">About Me</h5>
-		<div class="about-top"></div>
+		<div class="about-me">
+			<div class="me-pic">
+				<span class="avatar-pic"></span>
+			</div>
+			<div class="me-dsc">
+				<p class="me-title">My name is David Lau 
+					<span class="dsc-small">( LDW in chinese is short for Lau DeWei )</span>
+				</p>
+				<p class="me-content">I was born 1993 in GuangZhou , as my first time met with computer i was obessed with it , but off cause my parent had  prevented me to indulge in it . So far , i am a <strong>Font-End Engineer ^_^</strong></p>
+			</div>
+		</div>
+		<h5 class="about-title">About My Skill</h5>
+		<div class="about-skill">
+			
+		</div>
 	</div>
 </div>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -28,10 +28,15 @@ module.exports = (env) => {
 				},
 				{
 					test: /\.css$/,
-					use: ExtractTextPlugin.extract([
-					'style-loader',
-					'css-loader'
-					])
+					use: ExtractTextPlugin.extract({fallback:"style-loader",
+						use:[
+		                        {
+		                            loader: 'css-loader',
+		                            options:{
+		                                minimize: true //css压缩
+		                            }
+		                        }
+                    	]})
 				},
 				{
 					test :/\.less$/,
